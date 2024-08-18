526d000d7dd0c5de20936bbe120bba99ed0d27a8
Ensor Palacios <yq18021@bristol.ac.uk>
2024-08-18 13:29:41 +0100

    nvim-tree added open keymaps, added my_on_attach, changed default 'change_root_to_parent' 'Up'

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,3 +1,19 @@
+local function my_on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts 'Up') -- changed from default <C-t>
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+end
+
+-- pass to setup along with your other options
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
@@ -6,6 +22,21 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('nvim-tree').setup {}
+    require('nvim-tree').setup {
+      vim.keymap.set('n', '<leader>st', '<cmd>NvimTreeOpen<cr>'),
+      sort = {
+        sorter = 'case_sensitive',
+      },
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+      on_attach = my_on_attach,
+    }
   end,
 }
